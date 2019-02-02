dd1c75cd8bc755de81362012ddd76f618d48e833
Dylan Gyesbreghs <dgyesbreghs@gmail.com>
2019-02-02 21:10:44 +0100

    Add extra tests

--- Tests/HTTPMethodTests.swift
+++ Tests/HTTPMethodTests.swift
@@ -1,32 +1,26 @@
 //
 //  HTTPMethodTests.swift
-//  Amrap iOS Tests
+//  Amrap Tests
 //
 //  Created by Dylan Gyesbreghs on 02/02/2019.
 //
 
 import XCTest
 
-class HTTPMethodTests: XCTestCase {
+@testable import Amrap
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+class HttpMethodTests: XCTestCase {
+
+    func testGet() {
+        XCTAssertEqual(HttpMethod.get.rawValue, "GET")
     }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    func testHead() {
+        XCTAssertEqual(HttpMethod.head.rawValue, "HEAD")
     }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testPost() {
+        XCTAssertEqual(HttpMethod.post.rawValue, "POST")
     }
 
 }
