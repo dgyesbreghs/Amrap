1d785630b1d1f7abc1fc4455138947d132167967
Dylan Gyesbreghs <dgyesbreghs@gmail.com>
2019-02-02 20:48:21 +0100

    First real commit

---
+++ Amrap macOS/Amrap_macOS.h
@@ -0,0 +1,18 @@
+//
+//  Amrap_macOS.h
+//  Amrap macOS
+//
+//  Created by Dylan Gyesbreghs on 02/02/2019.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Amrap_macOS.
+FOUNDATION_EXPORT double Amrap_macOSVersionNumber;
+
+//! Project version string for Amrap_macOS.
+FOUNDATION_EXPORT const unsigned char Amrap_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Amrap_macOS/PublicHeader.h>
+
+
---
+++ Amrap tvOS/Amrap_tvOS.h
@@ -0,0 +1,18 @@
+//
+//  Amrap_tvOS.h
+//  Amrap tvOS
+//
+//  Created by Dylan Gyesbreghs on 02/02/2019.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Amrap_tvOS.
+FOUNDATION_EXPORT double Amrap_tvOSVersionNumber;
+
+//! Project version string for Amrap_tvOS.
+FOUNDATION_EXPORT const unsigned char Amrap_tvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Amrap_tvOS/PublicHeader.h>
+
+
