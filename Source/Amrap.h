e3c663b2743a1fe14699b0203d9ce0770d306946
Dylan Gyesbreghs <dgyesbreghs@gmail.com>
2019-02-02 20:48:54 +0100

    Real files

---
+++ Source/Amrap.h
@@ -0,0 +1,18 @@
+//
+//  Amrap.h
+//  Amrap
+//
+//  Created by Dylan Gyesbreghs on 02/02/2019.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Amrap.
+FOUNDATION_EXPORT double AmrapVersionNumber;
+
+//! Project version string for Amrap.
+FOUNDATION_EXPORT const unsigned char AmrapVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Amrap/PublicHeader.h>
+
+
